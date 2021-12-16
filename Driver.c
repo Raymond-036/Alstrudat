6ab19ecceeb18683599cfba4fe7952964f0fee78
Sopianna Siagian <82864489+Sopianna082@users.noreply.github.com>
2021-12-16 23:23:05 +0700

    Merge branch 'main' of https://github.com/Raymond-036/Alstrudat

---
+++ Driver.c
@@ -0,0 +1,23 @@
+/*
+Tugas Besar Alstrudat
+Kelompok 3
+Anggota :
+11420082 Sopianna Siagian
+11420049 Simon Natanael Siahaan
+11420054 CASANDRA RISYAH MARIA NAPITUPULU
+11420036 Raymond G. Saor Simamora
+11420070 Boby Heryanto H.S
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<conio.h>
+#include<windows.h>
+#include "Login.c"
+
+int main(){	
+	printf("=======Selamat Datang Di Aplikasi!=========\n");
+	menu_utama();
+	return 0;
+}
--- login.c
+++ login.c
@@ -1,3 +1,14 @@
+/*
+Tugas Besar Alstrudat
+Kelompok 3
+Anggota :
+11420082 Sopianna Siagian
+11420049 Simon Natanael Siahaan
+11420054 CASANDRA RISYAH MARIA NAPITUPULU
+11420036 Raymond G. Saor Simamora
+11420070 Boby Heryanto H.S
+*/
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>

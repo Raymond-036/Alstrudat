99678ef71555d4ad8d504b5dc32b86d2d76608ac
Raymond-036 <saorsimamora123@gmail.com>
2021-12-16 22:29:29 +0700

    Merge branch 'main' of https://github.com/Raymond-036/Alstrudat

---
+++ driver.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<conio.h>
+#include<windows.h>
+#include "Login.c"
+
+int main(){	
+	printf("=======Selamat Datang Di Aplikasi!=========\n");
+	menu_utama();
+	return 0;
+}
